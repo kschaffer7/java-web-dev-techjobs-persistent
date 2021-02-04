a149aa2eac67490661d8747130f4a1ef0ba6d1ac
Kirsti Schaffer <kirstischaffer@gmail.com>
2021-02-03 19:59:23 -0600

    Part 2 models complete?

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+// Employer is a class that will be mapped to one of our tables.
+    // Make sure the class has the @Entity annotation,
+    // as well as the no-arg constructor required for Hibernate to create an object.
+@Entity
 public class Employer extends AbstractEntity {
 
-
+    // In addition to the fields inherited from AbstractEntity,
+    // Employer should have a string field for location.
+    // Add the field for location with validation.
+    // In addition, add getters and setters to Employer.
+    @NotBlank(message = "Location is required")
+    @Size(min = 1, max = 200, message = "Location must be between 1 and 200 characters")
+    private String location;
+    // the no-arg constructor required for Hibernate to create an object.
+    public Employer () {}
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+// In the model class Skill, add a field for a longer description of the skill.
+// Some hiring managers like to have more information available about the nature of a given programming language or framework.
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+// As with Employer, give this class the @Entity annotation and be sure it contains a no-arg constructor.
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Size(min = 10, max = 1000, message = "Long Description must be between 10 and 1000 characters")
+    private String longDescription;
+
+    // the no-arg constructor required for Hibernate to create an object.
+    public Skill () {}
 
 }
