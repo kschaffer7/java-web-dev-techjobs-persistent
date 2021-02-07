0ac1ef4d218cf264b9b94ea1d5f1447d42e019f9
Kirsti Schaffer <kirstischaffer@gmail.com>
2021-02-06 20:34:08 -0600

    Part 2 complete. Queries 1 and 2 completed.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,10 @@
 ## Part 1: Test it with SQL
+## SQL TASK: At this point, you will have one table, job. In queries.sql under “Part 1”, list the columns and their data types in the table.
+## SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'job';
 
 ## Part 2: Test it with SQL
+## write a query to list the names of the employers in St. Louis City
+## SELECT * FROM techjobs.employer WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
